7b80d57e4cb27d0a466f0413897998cd885a27bc
xFerchoVJ <fer_elsword@hotmail.com>
2025-01-09 00:36:28 -0600

    ADD cors configuration

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,14 @@ module BackendContalink
     
     # Configuration jobs
     config.active_job.queue_adapter = :sidekiq
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:4200'
+        resource '*',
+          headers: :any,
+          methods: %i[get post put patch delete options head]
+      end
+    end
   end
 end
